7f317b5b3babab274a7021596e6404e93c2b8757
Lars <lvs1998@googlemail.com>
2018-06-13 16:06:13 +0200

    TTY - talking to yourself
    TTS, STT and chatbot implimented in the ExampleStreaming scene.

---
+++ TalkToYourself/Assets/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+
+	public static Singleton singleton;
+
+	public ExampleStreamingExp STT;
+	public AIMLTestChat BOT;
+	public ExampleTextToSpeech TTS;
+
+	void Awake(){
+		if (Singleton.singleton == null)
+			Singleton.singleton = this;
+		else
+			Destroy (this);
+		DontDestroyOnLoad (this);
+
+		STT = GameObject.Find ("STT").GetComponent<ExampleStreamingExp>();
+		BOT = GameObject.Find ("BOT").GetComponent<AIMLTestChat> ();
+		TTS = GameObject.Find ("TTS").GetComponent<ExampleTextToSpeech> ();
+
+	}
+
+	public void InputVoice(string text){
+		if (text == null || text == "")
+			return;
+		string res = BOT.GetAnswer (text);
+		Debug.Log (res);
+		TTS.Speak (res);
+	}
+
+}
